4814edc1f56b90b229e404a4e156d8a44d873e5a
Jorge Díaz <jorgediazsamaniego@gmail.com>
2019-07-25 01:25:44 -0400

    Options styles

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 import Option from './Option'
+import '../styles/Options.css'
 
-class Options extends Component {
-  render() {
-    const { options, handleAddOption } = this.props
-    return (
-      <Form.Group>
-        {options.length > 0 && options.map((option, index) => <Option key={index} option={option} index={index} {...this.props} />)}
-        <Button variant="primary" onClick={() => handleAddOption()}>
-          Add Option
-        </Button>
-      </Form.Group>
-    )
-  }
+export default function Options(props) {
+  const { options, handleAddOption } = props
+
+  return (
+    <Form.Group className="options">
+      {options.length > 0 && options.map((option, index) => <Option key={index} option={option} index={index} {...props} />)}
+      <Button className="add-option" variant="primary" onClick={() => handleAddOption()}>
+        <FontAwesomeIcon icon={faPlus} /> Add Option
+      </Button>
+    </Form.Group>
+  )
 }
-
-export default Options
